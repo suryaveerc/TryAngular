4da368e8e0e3840266c3621e203484c10fdfbea3
suryaveer <suryaveer.chauhan@gmail.com>
2018-08-09 20:11:48 -0400

    Initial commit

---
+++ src/js/app/blog-list/blog-list.component.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('blogList').
+	controller('BlogListController',function(){
+		console.log("Hello from BlogListController")
+	});
+	//component(function(){});
---
+++ src/quicktips.md
@@ -0,0 +1,1 @@
+quicktips.md
